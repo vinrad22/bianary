92e8d874f16bdcc8a0654f3187e580e0c373bfa0
vince radlicz <vradlicz6377@stu.d214.org>
2024-09-17 08:56:46 -0500

    Initial Commit

---
+++ bianary/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  bianary
+//
+//  Created by Vincent M. Radlicz on 9/17/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ bianary/bianaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  bianaryApp.swift
+//  bianary
+//
+//  Created by Vincent M. Radlicz on 9/17/24.
+//
+
+import SwiftUI
+
+@main
+struct bianaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
