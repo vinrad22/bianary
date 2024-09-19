5179b6864e1648a2579ca67111f65de9573618dc
Victor Tabacoff <victortabacoff2007@gmail.com>
2024-09-19 09:02:33 -0500

    hoi

--- bianary/BiHex.swift
+++ bianary/BiHex.swift
@@ -1,32 +1,89 @@
-//
-//  BiHex.swift
-//  bianary
-//
-//  Created by Vincent M. Radlicz on 9/17/24.
-//
-
 import SwiftUI
 
 struct BiHex: View {
+    @State private var numberValue: String = ""
+    @State private var decimalValue: String = ""
+    @State private var binaryValue: String = ""
+    @State private var hexValue: String = ""
+    @State private var numberBValue: String = ""
+    @State private var decimalBValue: String = ""
+    @State private var binaryBValue: String = ""
+    @State private var hexBValue: String = ""
+    @State private var numberHValue: String = ""
+    @State private var decimalHValue: String = ""
+    @State private var binaryHValue: String = ""
+    @State private var hexHValue: String = ""
     var body: some View {
-        NavigationView {
-            VStack {
-                NavigationLink(destination: HexDec()) {
-                                        Text("Hexadecimal")
-                                            .foregroundColor(.black)
-                                            .frame(width: 200, height: 300)
-                                            .background(.red)
-                                            .cornerRadius(15)
-                                    }
-                
-                NavigationLink(destination: ContentView()) {
-                                        Text("Binary")
-                                            .foregroundColor(.black)
-                                            .frame(width: 200, height: 300)
-                                            .background(.red)
-                                            .cornerRadius(15)
-                                    }
+        VStack {
+            Text("Decimal to Binary and Hexadecimal")
+                .font(.system(size: 22))
+            TextField("Enter a number", text: $numberValue)
+                .keyboardType(.numberPad)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+            Button(action: convertNumberBH) {
+                Text("Convert")
+                    .padding()
+                    .background(Color.red)
+                    .foregroundColor(.white)
+                    .cornerRadius(8)
             }
+            Text("Decimal: \(decimalValue)")
+            Text("Binary: \(binaryValue)")
+            Text("Hexadecimal: \(hexValue)")
+            Divider()
+            Text("Binary to Decimal and Hexadecimal")
+                .font(.system(size: 22))
+            TextField("Enter a binary", text: $numberBValue)
+                .keyboardType(.numberPad)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+            Button(action: convertNumberDH) {
+                Text("Convert")
+                    .padding()
+                    .background(Color.green)
+                    .foregroundColor(.white)
+                    .cornerRadius(8)
+            }
+            Text("Decimal: \(decimalBValue)")
+            Text("Binary: \(binaryBValue)")
+            Text("Hexadecimal: \(hexBValue)")
+            Divider()
+            Text("Hexadecimal to Decimal and Binary")
+                .font(.system(size: 22))
+            TextField("Enter a hexadecimal", text: $numberHValue)
+                .keyboardType(.numberPad)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+            Button(action: convertNumberBD) {
+                Text("Convert")
+                    .padding()
+                    .background(Color.blue)
+                    .foregroundColor(.white)
+                    .cornerRadius(8)
+            }
+            Text("Decimal: \(decimalHValue)")
+            Text("Binary: \(binaryHValue)")
+            Text("Hexadecimal: \(hexHValue)")
+        }
+        .padding()
+    }
+    func convertNumberBH() {
+        if let number = Int(numberValue) {
+            decimalValue = String(number, radix: 10)
+            binaryValue = String(number, radix: 2)
+            hexValue = String(number, radix: 16)
+        }
+    }
+    func convertNumberDH() {
+        if let numberB = Int(numberBValue, radix: 2) {
+            decimalBValue = String(numberB, radix: 10)
+            binaryBValue = String(numberB, radix: 2)
+            hexBValue = String(numberB, radix: 16)
+        }
+    }
+    func convertNumberBD() {
+        if let numberB = Int(numberHValue, radix: 16) {
+            decimalHValue = String(numberB, radix: 10)
+            binaryHValue = String(numberB, radix: 2)
+            hexHValue = String(numberB, radix: 16)
         }
     }
 }
